c9a2615017f0a73f970705177344e99663afb9e5
Dang Quang <quangdang229@gmail.com>
2022-06-16 00:31:45 +0700

    modified to submit exercise 1.3 of part 2

--- CIS419-master/Assignment1/hw1_skeleton/dtree_eval.py
+++ CIS419-master/Assignment1/hw1_skeleton/dtree_eval.py
@@ -11,7 +11,7 @@ from sklearn.metrics import accuracy_score
 
 
 
-def evaluatePerformance():
+def evaluatePerformance(numTrials = 100):
     '''
     Evaluate the performance of decision trees,
     averaged over 1,000 trials of 10-fold cross validation
@@ -35,19 +35,57 @@ def evaluatePerformance():
     y = np.array([data[:, 0]]).T
     n,d = X.shape
 
-    # shuffle the data
-    idx = np.arange(n)
-    np.random.seed(13)
-    np.random.shuffle(idx)
-    X = X[idx]
-    y = y[idx]
-    
-    # split the data
-    Xtrain = X[1:101,:]  # train on first 100 instances
-    Xtest = X[101:,:]
-    ytrain = y[1:101,:]  # test on remaining instances
-    ytest = y[101:,:]
+    # run 100 trials
+    folds = 10
+    accuracyResults = []
+    stumpAccuracyResults = []
+    DT3AccuracyResults = []
 
+    for t in range(numTrials):
+        # shuffle the data at the beggining of each trial
+        idx = np.arange(n)
+        np.random.seed(13)
+        np.random.shuffle(idx)
+        X = X[idx]
+        y = y[idx]
+
+        clf = tree.DecisionTreeClassifier()
+        clfStump = tree.DecisionTreeClassifier(max_depth=1)
+        clfDT3 = tree.DecisionTreeClassifier(max_depth=3)
+    
+        # split the data
+        for i in range(folds):
+            start = int(round((len(X)/10.0)*i))
+            stop = int(round((len(X)/10.0)*(i+1)))
+
+            XFirstHalf = X[0:start, :]
+            XSecondHalf = X[stop:, :]
+
+            # Xtrain = X[1:101,:]  # train on first 100 instances
+            Xtrain = np.concatenate((XFirstHalf,XSecondHalf))
+            Xtest = X[start:stop, :]
+
+            yFirstHalf = y[0:start, :]
+            ySecondHalf = y[stop:, :]
+
+            # ytrain = y[1:101,:]  # test on remaining instances
+            # ytest = y[101:,:]
+            ytrain = np.concatenate((yFirstHalf,ySecondHalf))
+            ytest = y[start:stop, :]
+
+        #unbounded lvl
+            clf = clf.fit(Xtrain,ytrain)
+            y_pred = clf.predict(Xtest)
+            accuracyResults.append(accuracy_score(ytest, y_pred))
+
+        #stump
+            clfStump = clfStump.fit(Xtrain, ytrain)
+            y_predStump = clfStump.predict(Xtest)
+            stumpAccuracyResults.append(accuracy_score(ytest, y_predStump))
+        #lvl 3
+            clfDT3 = clfDT3.fit(Xtrain, ytrain)
+            y_predDT3 = clfDT3.predict(Xtest)
+            DT3AccuracyResults.append(accuracy_score(ytest, y_predDT3))
     # train the decision tree
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(Xtrain,ytrain)
@@ -60,11 +98,14 @@ def evaluatePerformance():
     
     
     # TODO: update these statistics based on the results of your experiment
-    stddevDecisionTreeAccuracy = 0
-    meanDecisionStumpAccuracy = 0
-    stddevDecisionStumpAccuracy = 0
-    meanDT3Accuracy = 0
-    stddevDT3Accuracy = 0
+    meanDecisionTreeAccuracy = np.mean(accuracyResults)
+    stddevDecisionTreeAccuracy = np.std(accuracyResults)
+
+    meanDecisionStumpAccuracy = np.mean(stumpAccuracyResults)
+    stddevDecisionStumpAccuracy = np.std(stumpAccuracyResults)
+
+    meanDT3Accuracy = np.mean(DT3AccuracyResults)
+    stddevDT3Accuracy = np.std(DT3AccuracyResults)
 
     # make certain that the return value matches the API specification
     stats = np.zeros((3,2))
@@ -82,7 +123,7 @@ def evaluatePerformance():
 if __name__ == "__main__":
     
     stats = evaluatePerformance()
-    print "Decision Tree Accuracy = ", stats[0,0], " (", stats[0,1], ")"
-    print "Decision Stump Accuracy = ", stats[1,0], " (", stats[1,1], ")"
-    print "3-level Decision Tree = ", stats[2,0], " (", stats[2,1], ")"
+    print ("Decision Tree Accuracy = ", stats[0,0], " (", stats[0,1], ")")
+    print ("Decision Stump Accuracy = ", stats[1,0], " (", stats[1,1], ")")
+    print ("3-level Decision Tree = ", stats[2,0], " (", stats[2,1], ")")
 # ...to HERE.
